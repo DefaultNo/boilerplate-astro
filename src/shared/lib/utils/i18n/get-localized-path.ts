68876ad02aa80cf5b75ee5e1c96d0c8189ccc40d
DefaultNo <dmitriy09050@gmail.com>
2025-11-15 19:58:12 +0200

    fix(paths): Use path - `@/config/i18n`

--- src/shared/lib/utils/i18n/get-localized-path.ts
+++ src/shared/lib/utils/i18n/get-localized-path.ts
@@ -1,4 +1,4 @@
-import type { Locale } from '@/shared/config/i18n'
+import type { Locale } from '@/config/i18n'
 
 export function getLocalizedPath(locale: Locale, path: string = ''): string {
   const cleanPath = path.startsWith('/') ? path.slice(1) : path
--- src/shared/lib/utils/i18n/use-translations.ts
+++ src/shared/lib/utils/i18n/use-translations.ts
@@ -1,4 +1,4 @@
-import type { Locale } from '@/shared/config/i18n'
+import type { Locale } from '@/config/i18n'
 import type { Resources } from '@/shared/config/i18n/generated/resources.types'
 
 const translationsCache = new Map<string, any>()
